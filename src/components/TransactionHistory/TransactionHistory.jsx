61416cf6e4a453f0236dd8dde12f84033e024679
Inna-Kovalishina <114028931+Inna-Kovalishina@users.noreply.github.com>
2023-04-30 18:06:30 +0300

    adds PropTypes to TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export const TransactionHistory = ({items}) => {
     return (
@@ -22,4 +22,13 @@ export const TransactionHistory = ({items}) => {
             </tbody>
         </table>
     )
+}
+
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired,
+    }))
 }
